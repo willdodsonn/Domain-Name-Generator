7b34e8fc9d895dd79d6b58a450ee865ec56ddb1f
willdodsonn <sodlax38@gmail.com>
2022-02-01 01:29:18 +0000

    domain name

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our", "your"];
+let adj = ["great", "cold", "new"];
+let noun = ["duncecap", "load", "grass"];
